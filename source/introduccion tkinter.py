f80501eb30753b5094f983e7e8417e2de5400fed
Gustavo Adolfo Martinez Enriquez <25460323@colima.tecnm.mx>
2025-11-19 15:49:24 -0600

    Agregamos una ventana de tkinter

---
+++ source/introduccion tkinter.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def mostrar_mensaje():
+    messagebox.showinfo("Aviso", "Boton presionado")
+
+ventana = tk.Tk()
+ventana.title("Ventana simple")
+
+label=tk.Label(ventana,text="Presiona el boton para ver un mensaje")
+label.pack(pady=10)
+
+label.pack()
+
+boton = tk.Button(ventana,text= "Haz click aqui", command=mostrar_mensaje)
+boton.pack(pady=10)
+
+ventana.mainloop()
+
